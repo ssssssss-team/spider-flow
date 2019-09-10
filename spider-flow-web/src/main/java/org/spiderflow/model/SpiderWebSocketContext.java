77e19d41dd06a5c6034b918e7d014ce86478d1aa
mxd <838425805@qq.com>
2019-09-10 13:41:58 +0800

    json long类型转String

--- spider-flow-web/src/main/java/org/spiderflow/model/SpiderWebSocketContext.java
+++ spider-flow-web/src/main/java/org/spiderflow/model/SpiderWebSocketContext.java
@@ -1,6 +1,9 @@
 package org.spiderflow.model;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Date;
 
 import javax.websocket.Session;
@@ -9,21 +12,34 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 import org.spiderflow.context.SpiderContext;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializeFilter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ValueFilter;
 
 /**
  * WebSocket通讯中爬虫的上下文域
  * @author Administrator
  *
  */
-public class SpiderWebSocketContext extends SpiderContext{
+public class SpiderWebSocketContext extends SpiderContext implements ObjectSerializer{
 
 	private static final long serialVersionUID = -1205530535069540245L;
 	
 	private Session session;
 	
+	private SerializeConfig serializeConfig;
+	
 	public SpiderWebSocketContext(Session session) {
 		super();
 		this.session = session;
+		this.serializeConfig = new SerializeConfig();
+		this.serializeConfig.put(Long.TYPE, this);
+		this.serializeConfig.put(Long.class, this);
+		this.serializeConfig.put(BigDecimal.class, this);
+		this.serializeConfig.put(BigInteger.class, this);
 	}
 
 	@Override
@@ -40,12 +56,25 @@ public class SpiderWebSocketContext extends SpiderContext{
 		synchronized (session) {
 			if (session.isOpen()) {
 				try {
-					session.getBasicRemote().sendText(JSON.toJSONString(event));
+					session.getBasicRemote().sendText(JSON.toJSONString(event,this.serializeConfig));
 				} catch (IOException e) {
 					//忽略异常
 				}
 			}
 		}
+	}
+
+	@Override
+	public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
+		if(object == null){
+			if(serializer.isEnabled(SerializerFeature.WriteNullNumberAsZero)){
+				serializer.out.write("0");
+			}else{
+				serializer.out.writeNull();
+			}
+			return;
+		}
+		serializer.out.writeString(object.toString());
 	}
 	
 }
