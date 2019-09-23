a06ee97271d8156bab9539646e4d4b1c4067caf7
zhouyi <865173901@qq.com>
2019-09-23 14:03:40 +0800

    修改Spider.java循环得到标签修改为Map.get获取

--- spider-flow-core/src/main/java/org/spiderflow/core/Spider.java
+++ spider-flow-core/src/main/java/org/spiderflow/core/Spider.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -23,12 +26,13 @@ import org.springframework.stereotype.Component;
 
 /**
  * 爬虫的核心类
+ * 
  * @author jmxd
  *
  */
 @Component
 public class Spider {
-	
+
 	/**
 	 * 节点执行器列表 当前爬虫的全部流程
 	 */
@@ -39,84 +43,89 @@ public class Spider {
 	 */
 	@Autowired
 	private ExpressionEngine engine;
-	
-	
+
 	@Autowired(required = false)
 	private List<SpiderListener> listeners;
-	
-	
-	public List<SpiderOutput> run(SpiderFlow spiderFlow,Map<String,Object> variables){
-		if(variables == null){
+
+	private static Map<String, ShapeExecutor> executorMap = new HashMap<String, ShapeExecutor>();
+
+	@PostConstruct
+	private void init() {
+		executorMap = executors.stream().collect(Collectors.toMap(ShapeExecutor::supportShape, v -> v));
+	}
+
+	public List<SpiderOutput> run(SpiderFlow spiderFlow, Map<String, Object> variables) {
+		if (variables == null) {
 			variables = new HashMap<>();
 		}
 		SpiderNode root = SpiderFlowUtils.loadXMLFromString(spiderFlow.getXml());
 		SpiderContext context = new SpiderContext();
-		executeRoot(root, context,variables);
+		executeRoot(root, context, variables);
 		return context.getOutputs();
 	}
-	
-	public List<SpiderOutput> run(SpiderFlow spiderFlow){
-		return run(spiderFlow,new HashMap<>());
+
+	public List<SpiderOutput> run(SpiderFlow spiderFlow) {
+		return run(spiderFlow, new HashMap<>());
 	}
-	
-	public List<SpiderOutput> runWithTest(SpiderNode root,SpiderContext context){
-		//开始不允许设置任何东西
-		executeRoot(root, context,new HashMap<>());
+
+	public List<SpiderOutput> runWithTest(SpiderNode root, SpiderContext context) {
+		// 开始不允许设置任何东西
+		executeRoot(root, context, new HashMap<>());
 		context.info("测试完毕！");
 		return context.getOutputs();
 	}
-	
-	private void executeRoot(SpiderNode root,SpiderContext context,Map<String,Object> variables){
+
+	private void executeRoot(SpiderNode root, SpiderContext context, Map<String, Object> variables) {
 		int nThreads = NumberUtils.toInt(root.getStringJsonValue(ShapeExecutor.THREAD_COUNT), 8);
 		ThreadPool pool = ThreadPool.create(nThreads);
 		context.setRootNode(root);
 		context.setThreadPool(pool);
-		if(listeners != null){
-			listeners.forEach(listener->listener.beforeStart(context));
+		if (listeners != null) {
+			listeners.forEach(listener -> listener.beforeStart(context));
 		}
 		try {
-			executeNode(pool,null,root,context,variables);
+			executeNode(pool, null, root, context, variables);
 			pool.shutdown();
 		} finally {
-			if(listeners != null){
-				listeners.forEach(listener->listener.afterEnd(context));
+			if (listeners != null) {
+				listeners.forEach(listener -> listener.afterEnd(context));
 			}
 			ExpressionHolder.remove();
 		}
 	}
-	
-	public void execute(int nThreads,SpiderNode fromNode,SpiderNode node,SpiderContext context,Map<String,Object> variables){
+
+	public void execute(int nThreads, SpiderNode fromNode, SpiderNode node, SpiderContext context, Map<String, Object> variables) {
 		ThreadPool pool = ThreadPool.create(nThreads);
 		context.setThreadPool(pool);
-		executeNode(pool,fromNode,node,context,variables);
+		executeNode(pool, fromNode, node, context, variables);
 		pool.shutdown();
 	}
-	
-	private void executeaNextNodes(ThreadPool pool,SpiderNode node,SpiderContext context,Map<String,Object> variables){
+
+	private void executeaNextNodes(ThreadPool pool, SpiderNode node, SpiderContext context, Map<String, Object> variables) {
 		List<SpiderNode> nextNodes = node.getNextNodes();
-		if(nextNodes != null){
+		if (nextNodes != null) {
 			for (SpiderNode nextNode : nextNodes) {
-				executeNode(pool,node,nextNode,context,variables);
+				executeNode(pool, node, nextNode, context, variables);
 			}
 		}
 	}
-	
-	public void executeNode(ThreadPool pool,SpiderNode fromNode,SpiderNode node,SpiderContext context,Map<String,Object> variables){
+
+	public void executeNode(ThreadPool pool, SpiderNode fromNode, SpiderNode node, SpiderContext context, Map<String, Object> variables) {
 		String shape = node.getStringJsonValue("shape");
-		if(StringUtils.isBlank(shape)){
+		if (StringUtils.isBlank(shape)) {
 			executeaNextNodes(pool, node, context, variables);
 			return;
 		}
-		if(!executeCondition(fromNode,node,context,variables)){
+		if (!executeCondition(fromNode, node, context, variables)) {
 			return;
 		}
-		context.debug("执行节点[{}:{}]", node.getNodeName(),node.getNodeId());
+		context.debug("执行节点[{}:{}]", node.getNodeName(), node.getNodeId());
 		int loopCount = 1;
 		String loopCountStr = node.getStringJsonValue(ShapeExecutor.LOOP_COUNT);
-		if(StringUtils.isNotBlank(loopCountStr)){
+		if (StringUtils.isNotBlank(loopCountStr)) {
 			Object result = engine.execute(loopCountStr, variables);
-			if(result != null){
-				context.debug("获取循环次数{}={}",loopCountStr,result);
+			if (result != null) {
+				context.debug("获取循环次数{}={}", loopCountStr, result);
 				try {
 					loopCount = Integer.valueOf(result.toString());
 				} catch (NumberFormatException e) {
@@ -124,53 +133,50 @@ public class Spider {
 				}
 			}
 		}
-		if(loopCount > 0){
+		if (loopCount > 0) {
 			String loopVariableName = node.getStringJsonValue(ShapeExecutor.LOOP_VARIABLE_NAME);
-			for (ShapeExecutor executor : executors) {
-				if(executor.supportShape().equals(shape)){
-					for (int i = 0; i < loopCount; i++) {
-						if(context.isRunning()){
-							//存入下标变量
-							Map<String, Object> nVariables = Maps.add(variables, loopVariableName, i);
-							Runnable runnable = ()->{
-								if(context.isRunning()){
-									try {
-										ExpressionHolder.setVariables(nVariables);
-										executor.execute(node, context,nVariables);
-									} catch (Exception e) {
-										context.error("执行节点[{}:{}]出错,异常信息：{}",node.getNodeName(),node.getNodeId(),e);
-									} finally{
-										context.debug("执行节点[{}:{}]完毕",node.getNodeName(),node.getNodeId());
-										//递归执行下一级
-										executeaNextNodes(pool, node, context, nVariables);
-									}
-								}
-							};
-							if(executor.isThread()){
-								pool.submit(runnable);
-							}else{
-								runnable.run();
+			ShapeExecutor executor = executorMap.get(shape);
+			for (int i = 0; i < loopCount; i++) {
+				if (context.isRunning()) {
+					// 存入下标变量
+					Map<String, Object> nVariables = Maps.add(variables, loopVariableName, i);
+					Runnable runnable = () -> {
+						if (context.isRunning()) {
+							try {
+								ExpressionHolder.setVariables(nVariables);
+								executor.execute(node, context, nVariables);
+							} catch (Exception e) {
+								context.error("执行节点[{}:{}]出错,异常信息：{}", node.getNodeName(), node.getNodeId(), e);
+							} finally {
+								context.debug("执行节点[{}:{}]完毕", node.getNodeName(), node.getNodeId());
+								// 递归执行下一级
+								executeaNextNodes(pool, node, context, nVariables);
 							}
 						}
+					};
+					if (executor.isThread()) {
+						pool.submit(runnable);
+					} else {
+						runnable.run();
 					}
 				}
 			}
 		}
 	}
-		
-	private boolean executeCondition(SpiderNode fromNode,SpiderNode node,SpiderContext context,Map<String,Object> variables){
-		if(fromNode != null){
+
+	private boolean executeCondition(SpiderNode fromNode, SpiderNode node, SpiderContext context, Map<String, Object> variables) {
+		if (fromNode != null) {
 			String condition = node.getCondition(fromNode.getNodeId());
-			if(StringUtils.isNotBlank(condition)){	//判断是否有条件
+			if (StringUtils.isNotBlank(condition)) { // 判断是否有条件
 				Object result = null;
 				try {
 					result = engine.execute(condition, variables);
 				} catch (Exception e) {
-					context.error("判断{}出错,异常信息：{}",condition,e);
+					context.error("判断{}出错,异常信息：{}", condition, e);
 				}
-				if(result != null){
+				if (result != null) {
 					boolean isContinue = "true".equals(result) || Objects.equals(result, true);
-					context.debug("判断{}={}",condition,isContinue);
+					context.debug("判断{}={}", condition, isContinue);
 					return isContinue;
 				}
 				return false;
