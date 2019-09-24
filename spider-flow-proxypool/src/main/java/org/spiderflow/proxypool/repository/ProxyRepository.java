1b0c1d381fa51e7c25b99d6d63ef9b63067e68f3
小东 <838425805@qq.com>
2019-09-24 18:32:10 +0800

    删除文件 spider-flow-proxypool

--- spider-flow-proxypool/src/main/java/org/spiderflow/proxypool/repository/ProxyRepository.java
+++
@@ -1,8 +0,0 @@
-package org.spiderflow.proxypool.repository;
-
-import org.spiderflow.proxypool.model.Proxy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProxyRepository extends JpaRepository<Proxy, Integer>{
-
-}
