edec64c61ba959a47c956f947ba00b3a61186919
吕金泽 <1098696801@qq.com>
2019-09-25 11:20:15 +0800

    优化测试连接请求体验 封装sf.ajax

--- spider-flow-web/src/main/resources/static/js/common.js
+++ spider-flow-web/src/main/resources/static/js/common.js
@@ -6,3 +6,42 @@ function getQueryString(name) {
     }
     return null;
 }
+var sf = {};
+sf.ajax = function(options){
+	var loading;
+	var loadingInterval;
+	var closeClear = function(){
+		layui.layer.close(loading);
+		clearInterval(loadingInterval);
+	}
+	var beginTime = +new Date();
+	var url = options.url;
+	var type = options.type;
+	var data = options.data;
+	var success = options.success;
+	var error = options.error;
+	$.ajax({
+		url:url,
+		type:type,
+		data:data,
+		success:function(result){
+			closeClear();
+			success && success(result);
+		},
+		error:function(errorInfo){
+			closeClear();
+			error && error(errorInfo);
+		},
+		beforeSend:function(){
+			loadingInterval = setInterval(function(){
+				var endTime = +new Date();
+				if((endTime-beginTime) > 500){
+					loading = layui.layer.load(1, {
+						shade: [0.1,'#fff']
+					});
+					clearInterval(loadingInterval);
+				}
+			},100);
+		}
+	})
+}
