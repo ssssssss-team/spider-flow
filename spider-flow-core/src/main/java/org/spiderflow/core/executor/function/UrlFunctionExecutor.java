5c9e972c10aa02137e35fe6f67eee98cf258bb9a
吕金泽 <1098696801@qq.com>
2019-09-28 19:59:28 +0800

    优化 url.getParameters 改名为 url.parameterMap 获取全部参数 添加 url.parameter 根据key获取参数

--- spider-flow-core/src/main/java/org/spiderflow/core/executor/function/UrlFunctionExecutor.java
+++ spider-flow-core/src/main/java/org/spiderflow/core/executor/function/UrlFunctionExecutor.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.spiderflow.annotation.Comment;
 import org.spiderflow.annotation.Example;
 import org.spiderflow.executor.FunctionExecutor;
@@ -14,8 +15,6 @@ import org.springframework.stereotype.Component;
 
 /**
  * url 按指定字符集进行编码/解码 默认字符集(UTF-8) 工具类 防止NPE 
- * @author Administrator
- *
  */
 @Component
 public class UrlFunctionExecutor implements FunctionExecutor{
@@ -25,17 +24,40 @@ public class UrlFunctionExecutor implements FunctionExecutor{
 		return "url";
 	}
 	
-	@Comment("获取url后面的参数")
-	@Example("${url.getParameters('http://www.baidu.com/s?wd=spider-flow')}")
-	public static Map<String,String> getParameters(String url){
+	@Comment("获取url参数")
+	@Example("${url.parameter('http://www.baidu.com/s?wd=spider-flow')}")
+	public static String parameter(String url,String key){
+		return parameterMap(url).get(key);
+	}
+	
+	@Comment("获取url全部参数")
+	@Example("${url.parameterMap('http://www.baidu.com/s?wd=spider-flow&abbr=sf')}")
+	public static Map<String,String> parameterMap(String url){
+		Map<String,String> map = new HashMap<String,String>();
 		int index = url.indexOf("?");
-        String param = url.substring(index+1);
-        String[] params = param.split("&");
-        Map<String,String> map = new HashMap<String,String>();
-        for (String item:params) {
-            String[] kv = item.split("=");
-            map.put(kv[0],kv[1]);
-        }
+		if(index != -1) {
+	        String param = url.substring(index+1);
+	        if(StringUtils.isNotBlank(param)) {
+		        String[] params = param.split("&");
+		        for (String item : params) {
+		            String[] kv = item.split("=");
+		            if(kv.length > 0) {
+		            	if(StringUtils.isNotBlank(kv[0])) {
+		            		String value = "";
+		            		if(StringUtils.isNotBlank(kv[1])) {
+		            			int kv1Index = kv[1].indexOf("#");
+		            			if(kv1Index != -1) {
+		            				value = kv[1].substring(0,kv1Index);
+		            			}else {
+		            				value = kv[1];
+		            			}
+		            		}
+		            		map.put(kv[0],value);
+		            	}
+		            }
+		        }
+	        }
+		}
 		return map;
 	}
 	
