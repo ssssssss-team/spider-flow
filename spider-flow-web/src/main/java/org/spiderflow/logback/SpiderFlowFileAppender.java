b6c0ead1b254c964d230dac0877a80615bad98df
mxd <838425805@qq.com>
2020-03-08 13:53:53 +0800

    fix #I1AXQK

--- spider-flow-web/src/main/java/org/spiderflow/logback/SpiderFlowFileAppender.java
+++ spider-flow-web/src/main/java/org/spiderflow/logback/SpiderFlowFileAppender.java
@@ -2,35 +2,51 @@ package org.spiderflow.logback;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.spi.DeferredProcessingAware;
+import ch.qos.logback.core.status.ErrorStatus;
 import org.spiderflow.context.SpiderContext;
 import org.spiderflow.context.SpiderContextHolder;
 import org.spiderflow.core.job.SpiderJobContext;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 public class SpiderFlowFileAppender extends FileAppender<ILoggingEvent> {
 
-	private OutputStream baseOutputStream;
+    @Override
+    protected void subAppend(ILoggingEvent event) {
+        SpiderContext context = SpiderContextHolder.get();
+        OutputStream os = getOutputStream();
+        if (context instanceof SpiderJobContext) {
+            SpiderJobContext jobContext = (SpiderJobContext) context;
+            os = jobContext.getOutputstream();
+        }
+        try {
+            if (event instanceof DeferredProcessingAware) {
+                ((DeferredProcessingAware) event).prepareForDeferredProcessing();
+            }
+            byte[] byteArray = this.encoder.encode(event);
+            writeBytes(os, byteArray);
 
-	@Override
-	protected void subAppend(ILoggingEvent event) {
-		SpiderContext context = SpiderContextHolder.get();
-		if(context instanceof SpiderJobContext){
-			SpiderJobContext jobContext = (SpiderJobContext) context;
-			if(jobContext.getOutputstream() != getOutputStream()){
-				super.setOutputStream(jobContext.getOutputstream());
-			}
-		}else if(baseOutputStream != getOutputStream()){
-			super.setOutputStream(baseOutputStream);
-		}
-		super.subAppend(event);
-	}
+        } catch (IOException ioe) {
+            this.started = false;
+            addStatus(new ErrorStatus("IO failure in appender", this, ioe));
+        }
+    }
 
-	@Override
-	public void setOutputStream(OutputStream outputStream) {
-		if(baseOutputStream == null){
-			baseOutputStream = outputStream;
-		}
-		super.setOutputStream(outputStream);
-	}
+    private void writeBytes(OutputStream os, byte[] byteArray) throws IOException {
+        if (byteArray == null || byteArray.length == 0)
+            return;
+
+        lock.lock();
+        try {
+            os.write(byteArray);
+            if (isImmediateFlush()) {
+                os.flush();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
 }
